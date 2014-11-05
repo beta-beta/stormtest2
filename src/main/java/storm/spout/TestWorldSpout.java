375b20802705009170805da08269a3904935f977
beta-beta <hanwei4143@163.com>
2014-11-05 16:33:42 +0800

    hhhhh

---
+++ src/main/java/storm/spout/TestWorldSpout.java
@@ -0,0 +1,12 @@
+package storm.spout;
+
+import java.util.Map;
+import java.util.Random;
+
+import backtype.storm.spout.SpoutOutputCollector;
+/**
+ * Created by sina on 2014/11/5.
+ */
+public class TestWorldSpout {
+
+}
